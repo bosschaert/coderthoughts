a8e28ea4c06c365c2ea6b5e83a54e117087c50fd
David Bosschaert <david.bosschaert@gmail.com>
2011-03-30 18:44:13 +0100

    Convert into Blueprint

--- AsciiGraphicsService/ascii-pics-consumer-bundle/src/org/coderthoughts/asciipics/client/ClientComponent.java
+++ AsciiGraphicsService/ascii-pics-consumer-bundle/src/org/coderthoughts/asciipics/client/ClientComponent.java
@@ -1,50 +1,41 @@
 package org.coderthoughts.asciipics.client;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.coderthoughts.asciipics.api.PictureService;
 
 public class ClientComponent {
-    List<PictureService> services = new ArrayList<PictureService>();
+    private PictureService pictureService;
     private volatile boolean keepRunning;
 
     public void activate() {
+        System.out.println("************************ activated");
+
         keepRunning = true;
         new Thread(new DilbertPrinter()).start();
     }
 
     public void deactivate() {
+        System.out.println("####################### deactivated");
+
         keepRunning = false;
     }
 
-    public void addPictureService(PictureService ps) {
-        synchronized (services) {
-            services.add(ps);
-        }
+    public PictureService getPictureService() {
+        return pictureService;
     }
 
-    public void removePictureService(PictureService ps) {
-        synchronized (services) {
-            services.remove(ps);
-        }
+    public void setPictureService(PictureService ps) {
+        pictureService = ps;
     }
 
     private class DilbertPrinter implements Runnable {
         @Override
         public void run() {
             while (keepRunning) {
-                PictureService ps = null;
-                synchronized (services) {
-                    if (services.size() > 0) {
-                        ps = services.get(0);
-                    }
-                }
-                if (ps != null) {
-                    System.out.println();
-                    System.out.println(ps.getPic("Dilbert"));
-                }
+                System.out.println();
+                System.out.println(pictureService.getPic("Dilbert"));
+
                 try {
                     TimeUnit.SECONDS.sleep(5);
                 } catch (InterruptedException e) {
