9fd09416cdd600553a2b8ac5e268f1925e8c70bc
David Bosschaert <david.bosschaert@gmail.com>
2012-01-23 11:00:39 +0000

    Basic MSC service with JNDI

--- BasicMSC/src/main/java/org/coderthoughts/msc/MSCServiceActivator.java
+++ BasicMSC/src/main/java/org/coderthoughts/msc/MSCServiceActivator.java
@@ -1,5 +1,13 @@
 package org.coderthoughts.msc;
 
+import java.util.Hashtable;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NameClassPair;
+import javax.naming.NamingEnumeration;
+import javax.naming.NamingException;
+
 import org.jboss.msc.service.ServiceActivator;
 import org.jboss.msc.service.ServiceActivatorContext;
 import org.jboss.msc.service.ServiceRegistryException;
@@ -7,5 +15,21 @@ import org.jboss.msc.service.ServiceRegistryException;
 public class MSCServiceActivator implements ServiceActivator {
     public void activate(ServiceActivatorContext context) throws ServiceRegistryException {
         System.out.println("*** Activating MSC service: " + getClass().getName());
+
+        try {
+          Hashtable env = new Hashtable();
+          env.put(Context.INITIAL_CONTEXT_FACTORY, "org.jboss.as.naming.InitialContextFactory");
+          InitialContext ctx = new InitialContext(env);
+          System.out.println("@@@ CTX:" + ctx);
+          System.out.println("###" + ctx.getEnvironment());
+
+          NamingEnumeration<NameClassPair> res = ctx.list("");
+          while(res.hasMore()) {
+            System.out.println("@@@" + res.next());
+          }
+          res.close();
+        } catch (NamingException e) {
+          e.printStackTrace();
+        }
     }
 }
