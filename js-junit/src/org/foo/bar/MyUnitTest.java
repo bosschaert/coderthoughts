82d90d14b1e3c2a4047d13633d9b91c30fd9e830
David Bosschaert <david.bosschaert@gmail.com>
2014-09-25 21:32:20 +0200

    Fix tabs in code

--- js-junit/src/org/foo/bar/MyUnitTest.java
+++ js-junit/src/org/foo/bar/MyUnitTest.java
@@ -13,13 +13,12 @@ import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import org.junit.Test;
 
 public class MyUnitTest {
+    @Test
+    public void testSimpleJSFunction() throws Exception {
+        assertEquals("2", testJavaScript("function testFunction() { return 1+1; }"));
+    }
 
-	@Test
-	public void testSimpleJSFunction() throws Exception {
-	    assertEquals("2", testJavaScript("function testFunction() { return 1+1; }"));
-	}
-
-	@Test
+    @Test
     public void testAJAX() throws Exception {
         String jsResult = testJavaScript("function testFunction() { "
                 + "var xmlhttp = new XMLHttpRequest();"
@@ -42,41 +41,41 @@ public class MyUnitTest {
         return testJavaScript(script, false);
     }
 
-	public String testJavaScript(String script, boolean async) throws Exception {
-		StringBuilder html = new StringBuilder();
-		html.append("<!DOCTYPE html><html>"
-				+ "<body onload='myFunction()'>"
-				+ "<script>"
-				+ "function myFunction() { "
-				+ "  var res = testFunction();"
-				+ "  if (!(res === undefined)) {"
-				+ "    document.getElementById('test_result').innerHTML = res;"
-				+ "  }"
-				+ "}");
-		html.append(script);
-		html.append("</script>"
-				+ "<p id='test_result'>undefined</p>"
-				+ "</body>"
-				+ "</html>");
+    public String testJavaScript(String script, boolean async) throws Exception {
+        StringBuilder html = new StringBuilder();
+        html.append("<!DOCTYPE html><html>"
+                + "<body onload='myFunction()'>"
+                + "<script>"
+                + "function myFunction() { "
+                + "  var res = testFunction();"
+                + "  if (!(res === undefined)) {"
+                + "    document.getElementById('test_result').innerHTML = res;"
+                + "  }"
+                + "}");
+        html.append(script);
+        html.append("</script>"
+                + "<p id='test_result'>undefined</p>"
+                + "</body>"
+                + "</html>");
 
-		File f = File.createTempFile("jstest-", ".tmp");
+        File f = File.createTempFile("jstest-", ".tmp");
 
-		try (OutputStream fos = new FileOutputStream(f)) {
-		    fos.write(html.toString().getBytes());
-		    fos.close();
+        try (OutputStream fos = new FileOutputStream(f)) {
+            fos.write(html.toString().getBytes());
+            fos.close();
 
-    		WebClient wc = new WebClient();
-    		HtmlPage page = wc.getPage(f.toURI().toURL());
+            WebClient wc = new WebClient();
+            HtmlPage page = wc.getPage(f.toURI().toURL());
 
-    		String result = page.getHtmlElementById("test_result").asText();
-    		int count = 30;
-    		while (count-- > 0 && "undefined".equals(result)) {
-    		    Thread.sleep(500);
-    		    result = page.getHtmlElementById("test_result").asText();
-    		}
+            String result = page.getHtmlElementById("test_result").asText();
+            int count = 30;
+            while (count-- > 0 && "undefined".equals(result)) {
+                Thread.sleep(500);
+                result = page.getHtmlElementById("test_result").asText();
+            }
             return result;
-		} finally {
-		    f.delete();
-		}
-	}
+        } finally {
+            f.delete();
+        }
+    }
 }
