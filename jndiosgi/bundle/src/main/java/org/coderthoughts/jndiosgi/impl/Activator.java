31b2b4379ecf6131a15ff0927217c45590aca187
David Bosschaert <david.bosschaert@gmail.com>
2015-04-16 22:43:47 +0200

    Some temp changes to jndiosgi module

--- jndiosgi/bundle/src/main/java/org/coderthoughts/jndiosgi/impl/Activator.java
+++ jndiosgi/bundle/src/main/java/org/coderthoughts/jndiosgi/impl/Activator.java
@@ -2,20 +2,35 @@ package org.coderthoughts.jndiosgi.impl;
 
 import javax.naming.InitialContext;
 
-import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
 public class Activator implements BundleActivator {
 
     public void start(BundleContext context) throws Exception {
-        System.out.println("*********** hi there");
+        System.out.println("@*********** hi there");
 
-        for (Bundle b : context.getBundles()) {
-            System.out.println("Bundle: " + b.getSymbolicName());
+//        for (Bundle b : context.getBundles()) {
+//            System.out.println("Bundle: " + b.getSymbolicName());
+//        }
+
+        try {
+            InitialContext ctx = new InitialContext();
+//            System.out.println("Initial Context: " + ctx);
+            Object ut = ctx.lookup("UserTransaction");
+            System.out.println("Found ut: " + ut);
+            Object p = ctx.lookup("jdbc/db");
+            System.out.println("Found p: " + p);
+//            Object bc = ctx.lookup("osgi:framework/bundleContext");
+//            System.out.println("Found: " + bc);
+//            Object s = ctx.lookup("osgi:service/javax.transaction.UserTransaction/(component.name=UserTransaction)");
+//            System.out.println("Service: " + s);
+//            Object s2 = ctx.lookup("osgi:service/org.osgi.service.packageadmin.PackageAdmin");
+//            System.out.println("Service: " + s2);
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
-
-        InitialContext ctx = new InitialContext();
 //        System.out.println("$$: " + ctx.lookup("java:comp/env/jdbc/DefaultDatasource"));
 //        NamingEnumeration<NameClassPair> list = ctx.list("java:comp/env/");
 //        while (list.hasMore()) {
